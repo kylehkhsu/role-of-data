3e96f667419830db210b9a8d03efef65c78a249e
Kyle Hsu <kylehkhsu@gmail.com>
2019-10-07 14:15:09 -0400

    wandb scripts

---
+++ scripts/run_exp036.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+wandb login d34e5033d016b5e8f34841d773d02b48cbd74cc7
+wandb on
+wandb agent g4t3u9cg    # exp036
---
+++ scripts/run_exp037.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+wandb login d34e5033d016b5e8f34841d773d02b48cbd74cc7
+wandb on
+wandb agent p3z2byfj    # exp037
