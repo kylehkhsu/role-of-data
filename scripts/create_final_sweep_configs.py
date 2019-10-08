87cb7451934e096520b30ac1fbd7ff8e603f4f4e
Kyle Hsu <kylehkhsu@gmail.com>
2019-10-08 09:27:10 -0400

    more results

--- scripts/create_final_sweep_configs.py
+++ scripts/create_final_sweep_configs.py
@@ -2,19 +2,8 @@ import wandb_summarizer.download
 import ipdb
 import wandb
 import yaml
-n_seeds = 10
 
-api = wandb.Api()
-# experiment_type = 'direct'
-# runs = api.runs(
-#     'kylehsu/pacbayes_opt',
-#     {
-#         '$and': [
-#             {'tags': 'best'},
-#             {'tags': experiment_type}
-#         ]
-#     }
-# )
+n_seeds = 10
 
 exclude_keys = [
     '_wandb',
@@ -24,57 +13,39 @@ exclude_keys = [
     'dataset_path'
 ]
 
+
 def get_value(config, key):
     value = config[key]
     if type(value) is dict:
         value = value['value']  # happens when value is 0 for some reason
     return value
 
-# for run in runs:
-#     run_config = run.config
-#     config = {}
-#     config.update({'parameters': {key: {'value': get_value(run_config, key)} for key in run_config.keys() if key not in exclude_keys}})
-#     config['parameters'].update({'seed': {'values': [i + 1 for i in range(n_seeds)]}})
-#     config['parameters'].update({'bound_optimization_patience': {'value': 10}})
-#     alpha = get_value(run_config, 'alpha')
-#     dataset = get_value(run_config, 'dataset')
-#     sweep_name = f"exp-{experiment_type}__dataset-{dataset}__alpha-{alpha:.2f}"
-#     file_name = f"./sweeps/final/{sweep_name}.yaml"
-#     config.update({
-#         'name': sweep_name,
-#         'program': f'scripts/data_dependent_prior_{experiment_type}.py',
-#         'method': 'grid',
-#     })
-#
-#     with open(file_name, 'w') as f:
-#         yaml.dump(config, f)
-#     print(file_name)
 
-
-experiment_type = 'sgd'
+api = wandb.Api()
+experiment_type = 'direct'
 runs = api.runs(
     'kylehsu/pacbayes_opt',
     {
         '$and': [
             {'tags': 'best'},
-            {'tags': experiment_type},
+            {'tags': experiment_type}
         ]
     }
 )
+
 for run in runs:
     run_config = run.config
     if get_value(run_config, 'net_type') != 'mlp':
         continue
     config = {}
-    config.update({'parameters': {key: {'value': get_value(run_config, key)} for key in run_config.keys() if key not in exclude_keys}})
+    config.update({'parameters': {key: {'value': get_value(run_config, key)} for key in run_config.keys() if
+                                  key not in exclude_keys}})
     config['parameters'].update({'seed': {'values': [i + 1 for i in range(n_seeds)]}})
     # config['parameters'].update({'bound_optimization_patience': {'value': 10}})
-    config['parameters'].update({'oracle_prior_variance': {'values': [0, 1]}})
-    net_type = get_value(run_config, 'net_type')
     alpha = get_value(run_config, 'alpha')
     dataset = get_value(run_config, 'dataset')
-    posterior_mean_stopping_error_train = get_value(run_config, 'posterior_mean_stopping_error_train')
-    sweep_name = f"exp-{experiment_type}__dataset-{dataset}__alpha-{alpha:.1f}__net_type-{net_type}__posterior_mean_stopping_error_train-{posterior_mean_stopping_error_train}"
+    net_type = get_value(run_config, 'net_type')
+    sweep_name = f"exp-{experiment_type}__dataset-{dataset}__alpha-{alpha:.2f}__net_type-{net_type}"
     file_name = f"./sweeps/final/{sweep_name}.yaml"
     config.update({
         'name': sweep_name,
@@ -86,5 +57,38 @@ for run in runs:
         yaml.dump(config, f)
     print(file_name)
 
-
-
+# experiment_type = 'half_sgd'
+# runs = api.runs(
+#     'kylehsu/pacbayes_opt',
+#     {
+#         '$and': [
+#             {'tags': 'best'},
+#             {'tags': experiment_type},
+#         ]
+#     }
+# )
+# ipdb.set_trace()
+# for run in runs:
+#     run_config = run.config
+#     # if get_value(run_config, 'net_type') != 'mlp':
+#     #     continue
+#     config = {}
+#     config.update({'parameters': {key: {'value': get_value(run_config, key)} for key in run_config.keys() if key not in exclude_keys}})
+#     config['parameters'].update({'seed': {'values': [i + 1 for i in range(n_seeds)]}})
+#     # config['parameters'].update({'bound_optimization_patience': {'value': 10}})
+#     # config['parameters'].update({'oracle_prior_variance': {'values': [0, 1]}})
+#     net_type = get_value(run_config, 'net_type')
+#     alpha = get_value(run_config, 'alpha')
+#     dataset = get_value(run_config, 'dataset')
+#     posterior_mean_stopping_error_train = get_value(run_config, 'posterior_mean_stopping_error_train')
+#     sweep_name = f"exp-{experiment_type}__dataset-{dataset}__alpha-{alpha:.1f}__net_type-{net_type}__posterior_mean_stopping_error_train-{posterior_mean_stopping_error_train}"
+#     file_name = f"./sweeps/final/{sweep_name}.yaml"
+#     config.update({
+#         'name': sweep_name,
+#         'program': f'scripts/data_dependent_prior_{experiment_type}.py',
+#         'method': 'grid',
+#     })
+#
+#     with open(file_name, 'w') as f:
+#         yaml.dump(config, f)
+#     print(file_name)
--- scripts/create_wandb_sweeps.py
+++ scripts/create_wandb_sweeps.py
@@ -2,6 +2,11 @@ import os
 import ipdb
 import re
 
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--dry', type=int, default=1)
+args = parser.parse_args()
+
 yamls = os.popen('ls ./sweeps/final').read()
 yamls = yamls.split('\n')
 yamls = list(filter(None, yamls))
@@ -11,21 +16,27 @@ with open('./scripts/tests/wandb.txt', 'r') as f:
     wandb = f.read()
 
 dump = ''
+count = 0
 for yaml in yamls:
-    if 'net_type-mlp' not in yaml:
+    if 'ghost_sgd' not in yaml and 'half_sgd' not in yaml and \
+            not ('direct' in yaml and 'mlp' in yaml):
         continue
-    sweep_msg = os.popen(f'wandb sweep {yaml}').read()
+    count += 1
+    print(yaml)
+    if not args.dry:
+        sweep_msg = os.popen(f'wandb sweep {yaml}').read()
 
-    p = re.compile('.*Create sweep with ID: (\w+)\n.*', re.DOTALL)
-    id = p.match(sweep_msg)
-    if id:
-        id = id.group(1)
-    else:
-        print(f'regex failed to find id for yaml {yaml}')
-        break
-    dump += (f'wandb agent {id}\n')
+        p = re.compile('.*Create sweep with ID: (\w+)\n.*', re.DOTALL)
+        id = p.match(sweep_msg)
+        if id:
+            id = id.group(1)
+        else:
+            print(f'regex failed to find id for yaml {yaml}')
+            break
+        dump += (f'wandb agent {id}\n')
 
     # os.popen(f'srun --mem=12G -c 8 --gres=gpu:1 -p p100 wandb agent {id}')
-
-with open('./scripts/sgd_mnist_mlp_final.sh', 'w+') as f:
-    f.write(dump)
+print(f'count: {count}')
+if not args.dry:
+    with open('./scripts/mnist_ghost_half_direct-mlp_final.sh', 'w+') as f:
+        f.write(dump)
